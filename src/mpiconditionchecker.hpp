c4fe6724d8b46d4f6522a0d32955de6cac485e88
Jorge Bellon <jbellon@bsc.es>
2015-04-23 10:44:08 +0200

    Synchronized condition definition is now included from nanox-dev include files. Now all condition checker and synchronized conditions are template classes. This simplifies the code as we can switch from C to C++ MPI API functions just changing those templates. Now all MPI synchronized conditions are created in heap (rather than in the stack). This is because a condition may be satisfied after inserting it to the polling queue but before blocking the thread (inside Schedule::waitForCondition). In order to prevent memory leaks, and because we do always delete the MPI synchronized conditions from the same place, the pointers are encapsulated in a smart pointer (shared_pointer). This object will automatically delete the pointee object when it detects that the number of references reachs 0. It also must be used in the scheduler queue inside Nanos runtime.

--- src/mpiconditionchecker.hpp
+++ src/mpiconditionchecker.hpp
@@ -1,142 +1,186 @@
 #include <mpi.h>
-#include <cassert>
-#include <vector>
 
-#include <nanox-dev/synchronizedcondition_decl.hpp>
+#include <sstream>
+#include <nanox-dev/synchronizedcondition.hpp>
 #include <nanox-dev/schedule_decl.hpp>
 
 namespace nanos {
 
- void GenericSyncCond::lock()
-{
-   _lock.acquire();
-   memoryFence();
-}
-
- void GenericSyncCond::unlock()
-{
-   memoryFence();
-   _lock.release();
-}
-
-template <class _T>
- void SynchronizedCondition< _T>::wait()
-{
-   Scheduler::waitOnCondition(this);
-}
-
-template <class _T>
- void SynchronizedCondition< _T>::reference()
-{
-   _refcount++;
-   memoryFence();
-}
-
-template <class _T>
- void SynchronizedCondition< _T>::unreference()
-{
-   _refcount--;
-}
-
-template <class _T>
- void SynchronizedCondition< _T>::waitConditionAndSignalers()
-{
-   Scheduler::waitOnCondition(this);
-   while ( _refcount.value() > 0 ) {
-      memoryFence();
-   }
-}
-
-template <class _T>
- void SynchronizedCondition< _T>::signal()
-{
-   lock();
-   while ( hasWaiters() ) {
-      WD* wd = getAndRemoveWaiter();
-      Scheduler::wakeUp(wd);
-   }
-   unlock(); 
-}
-
-template <class _T>
- void SynchronizedCondition< _T>::signal_one()
-{
-   lock();
-   if ( hasWaiters() ) {
-      WD* wd = getAndRemoveWaiter();
-      Scheduler::wakeUp(wd);
-   }
-   unlock();
-}
-
 namespace mpi {
+
+#if 0
+// C++ Version of MPI_Test
+static bool test_request( MPI::Request &req, int* flag, MPI::Status &status )
+{
+    bool ret = req.Test( status );
+    *flag = (int) ret;
+    return ret;
+}
+
+// C++ Version of MPI_Testall
+static bool test_all_requests( size_t count, MPI::Request array_of_requests[], int *flag, MPI::Status array_of_statuses[] )
+{
+    bool ret = MPI::Request::Testall( count, array_of_requests, array_of_statuses );
+    *flag = (int) ret;
+    return ret;
+}
+#endif
+
+// C Version of MPI_Test
+static int test_request( MPI_Request *req, int *flag, MPI_Status *status )
+{
+    return MPI_Test( req, flag, status );
+}
+
+// C Version of MPI_Testall
+static int test_all_requests( size_t count, MPI_Request array_of_requests[], int *flag, MPI_Status array_of_statuses[] )
+{
+    return MPI_Testall( count, array_of_requests, flag, array_of_statuses );
+}
+
+template <typename Request, typename Status, typename Result>
 class SinglePendingCheck : ConditionChecker
 {
     private:
-        MPI_Request *_request;
-        MPI_Status  *_status;
+	typedef ConditionChecker super;
+        Request _request;
+        Status  _status;
+        Result  _result;
+	bool    _satisfied;
     public:
-        SinglePendingCheck ( MPI_Request *request, MPI_Status *status ) :
-            _request( request ), _status( status )
+        SinglePendingCheck ( Request request, Status status ) :
+            super(), _request( request ), _status( status ), _satisfied( false )
         {}
 
-        MPI_Request* getRequests() { return _request; }
-        MPI_Status* getStatuses() { return _status; }
-        size_t getSize() const { return 1; }
-        bool checkCondition () {
-            int flag;
-            MPI_Test( getRequests(), &flag, getStatuses() );
-            return flag == 1;
+	SinglePendingCheck ( SinglePendingCheck const& other ) :
+	    super( other ), _request(other._request), _status(other._status), 
+            _result(other._result), _satisfied( other._satisfied )
+        {}
+
+        virtual ~SinglePendingCheck() {}
+
+	SinglePendingCheck &operator=( SinglePendingCheck const& other ) {
+	    _request = other._request;
+	    _status = other._status;
+	    _result = other._result;
+            _satisfied = other._satisfied;
+	}
+
+        Request getRequest() { return _request; }
+        Status getStatus() { return _status; }
+        int getResult() const { return _result; }
+
+        virtual bool checkCondition () {
+            // We need to store the condition because it may not be safe
+            // to use the other members if a task was not finally blocked
+            // (i.e. the condition satisfied just before blocking the task)
+            if( !_satisfied ) {
+                int flag;
+                _result = test_request( _request, &flag, _status );
+                _satisfied = flag == 1;
+            }
+            return _satisfied;
         }
 };
 
+template <typename Request, typename Status, typename Result>
 class MultiplePendingCheck : ConditionChecker {
     private:
-        std::vector<MPI_Request> _requests;
-        std::vector<MPI_Status > _statuses;
+        typedef ConditionChecker super;
+        size_t   _length;
+        Request* _requests;
+        Status*  _statuses;
+        Result   _result;
+	bool     _satisfied;
     public:
-        MultiplePendingCheck ( std::initializer_list<MPI_Request> const& requests, std::initializer_list<MPI_Status> const& statuses ) :
-            _requests( requests ), _statuses( statuses)
+        MultiplePendingCheck ( size_t len, Request array_of_requests[], Status array_of_statuses[] ) :
+            super(), _length( len ), _requests( array_of_requests ), _statuses( array_of_statuses ),
+            _satisfied( false )
+        {}
+
+	MultiplePendingCheck ( MultiplePendingCheck const& other ) :
+	    super( other ), _length( other._length ), _requests(other._requests), 
+            _statuses(other._statuses), _result(other._result), _satisfied( other._satisfied )
+        {}
+
+	MultiplePendingCheck &operator=( MultiplePendingCheck const& other ) {
+	    _length = other._length;
+	    _requests = other._requests;
+	    _statuses = other._statuses;
+	    _result = other._result;
+            _satisfied = other._satisfied;
+	}
+
+	virtual ~MultiplePendingCheck() {}
+
+        Request* getRequests() { return _requests; }
+        Status* getStatuses() { return _statuses; }
+        Result getResult() const { return _result; }
+        size_t getSize() const { return _length; }
+
+        virtual bool checkCondition() {
+            // We need to store the condition because it may not be safe
+            // to use the other members if a task was not finally blocked
+            // (i.e. the condition satisfied just before blocking the task)
+            if( !_satisfied ) {
+                int flag;
+                _result = test_all_requests( _length, _requests, &flag, _statuses );
+                _satisfied = flag == 1;
+            }
+            return _satisfied;
+        }
+};
+
+// Template specifies the type of Request and Status objects
+// For C: Request = MPI_Request*, Status = MPI_Status*, Result: int
+// For C++: Request = Request&, Status = Status&, Result: bool
+template <typename Request, typename Status, typename Result>
+class SingleTestCond : public SingleSyncCond<SinglePendingCheck<Request,Status,Result> >
+{
+    typedef SinglePendingCheck<Request,Status,Result> CheckType;
+    typedef SingleSyncCond< CheckType > super;
+    public:
+        SingleTestCond( Request req, Status status ) :
+            super( CheckType( req, status ) )
+        {}
+
+        /*! \brief SingleTestCond destructor
+         */
+        virtual ~SingleTestCond() {}
+
+        Result getResult()
         {
-            assert( requests.size() == statuses.size() );
-        }
-
-        MPI_Request* getRequests() { return &_requests[0]; }
-        MPI_Status* getStatuses() { return &_statuses[0]; }
-        size_t getSize() const { return _requests.size(); }
-        bool checkCondition() {
-            int flag;
-            MPI_Testall( _requests.size(), getRequests(), &flag, getStatuses() );
-            return flag == 1;
+            return super::_conditionChecker.getResult();
         }
 };
 
-class SingleTestCond : public SingleSyncCond<SinglePendingCheck>
+// Template specifies the type of Request and Status objects
+// For C: Request = MPI_Request*, Status = MPI_Status*, Result: int
+// For C++: Request = Request&, Status = Status&, Result: bool
+template <typename Request, typename Status, typename Result>
+class MultipleTestCond : public SingleSyncCond<MultiplePendingCheck<Request,Status,Result> >
 {
-    typedef SingleSyncCond<SinglePendingCheck> super;
-	public:
-        SingleTestCond( MPI_Request *req, MPI_Status *status ) :
-            super( SinglePendingCheck( req, status ) )
+    typedef MultiplePendingCheck<Request,Status,Result> CheckType;
+    typedef SingleSyncCond< CheckType > super;
+    public:
+        MultipleTestCond ( size_t len, Request array_of_requests[], Status array_of_statuses[] ) :
+            super( CheckType( len, array_of_requests, array_of_statuses ) )
         {}
 
-		/*! \brief SingleTestCond destructor
-		 */
-		virtual ~SingleTestCond() { }
+        /*! \brief MultipleTestCond destructor
+         */
+        virtual ~MultipleTestCond() {}
+
+        Result getResult()
+        {
+            return super::_conditionChecker.getResult();
+        }
 };
 
-class MultipleTestCond : public SingleSyncCond<MultiplePendingCheck>
-{
-    typedef SingleSyncCond<MultiplePendingCheck> super;
-	public:
-        MultipleTestCond ( std::initializer_list<MPI_Request> const& requests, std::initializer_list<MPI_Status> const& statuses ) :
-            super( MultiplePendingCheck( requests, statuses ) )
-        {}
-
-		/*! \brief SingleTestCond destructor
-		 */
-		virtual ~MultipleTestCond() { }
-};
-
+typedef SingleTestCond<MPI_Request*,MPI_Status*,int> MPI_SingleTest;
+typedef MultipleTestCond<MPI_Request,MPI_Status,int> MPI_MultipleTest;
+typedef SingleTestCond<MPI::Request&,MPI::Status&,bool> SingleTest;
+typedef MultipleTestCond<MPI::Request,MPI::Status,bool> MultipleTest;
 
 }// namespace mpi
 }// namespace nanos
--- src/mpioverload.cc
+++ src/mpioverload.cc
@@ -1,5 +1,7 @@
 #include <mpi.h>
 #include <nanox/nanos.h>
+#include <nanox-dev/debug.hpp>
+#include <nanox-dev/smartpointer.hpp>
 #include "mpiconditionchecker.hpp"
 
 #if MPI_VERSION >= 3
@@ -10,9 +12,13 @@
 
 extern "C" {
 
+using namespace nanos;
+using namespace nanos::mpi;
+
 int MPI_Send( MPI3CONST void *buf, int count, MPI_Datatype datatype,
     int dest, int tag, MPI_Comm comm )
 {
+    debug0( "Intercepted " << __FUNCTION__ );
     int ierror = MPI_SUCCESS;
 
     MPI_Status status;
@@ -20,59 +26,37 @@ int MPI_Send( MPI3CONST void *buf, int count, MPI_Datatype datatype,
 
     ierror = MPI_Isend( buf, count, datatype, dest, tag, comm, &request );
 
-    nanos::mpi::SingleTestCond waitCond(&request, &status);
-    nanos_sync_cond_wait( &waitCond );
+    if( ierror == MPI_SUCCESS ) {
+        shared_pointer<MPI_SingleTest> waitCond = shared_pointer<MPI_SingleTest>(new MPI_SingleTest(&request, &status) );
+        nanos_sync_cond_wait( waitCond.getPointer() );
+
+        ierror = waitCond->getResult();
+    }
 
     //ierror = PMPI_Send(buf, count, datatype, dest, tag, comm);
-    return ierror + status.MPI_ERROR;
+    return ierror;
 }
 
 int MPI_Recv( void *buf, int count, MPI_Datatype datatype,
     int source, int tag, MPI_Comm comm, MPI_Status *status )
 {
+    debug0( "Intercepted " << __FUNCTION__ );
     int ierror = MPI_SUCCESS;
 
     MPI_Request request;
 
     ierror = MPI_Irecv( buf, count, datatype, source, tag, comm, &request );
-    std::cout << "Waiting for receive" << std::endl;
 
-    nanos::mpi::SingleTestCond waitCond(&request, status);
-    nanos_polling_cond_wait( &waitCond );
+    if( ierror == MPI_SUCCESS ) {
+        shared_pointer<MPI_SingleTest> waitCond = shared_pointer<MPI_SingleTest>(new MPI_SingleTest(&request, status) );
+        nanos_polling_cond_wait( waitCond.getPointer() );
+
+        ierror = waitCond->getResult();
+        //delete waitCond; Note: the scheduler will take care of deleting the condition
+    }
 
     //ierror = PMPI_Recv (buf, count, datatype, source, tag, comm, status );
-    return ierror + status->MPI_ERROR;
-}
-
-#if MPI_VERSION >=3
-
-int MPI_Barrier(MPI_Comm comm)
-{
-    int ierror = MPI_SUCCESS;
-    MPI_Request request;
-    MPI_Status status;
-
-    ierror = MPI_Ibarrier( comm, &request );
-
-    nanos::mpi::SingleTestCond waitCond(&request, &status);
-    nanos_polling_cond_wait( &waitCond );
-
-    return ierror + status.MPI_ERROR;
-}
-
-int MPI_Bcast(void *buffer, int count, MPI_Datatype datatype,
-    int root, MPI_Comm comm)
-{
-    int ierror = MPI_SUCCESS;
-    MPI_Request request;
-    MPI_Status status;
-
-    ierror = MPI_Ibcast( buffer, count, datatype, root, comm, &request );
-
-    nanos::mpi::SingleTestCond waitCond( &request, &status );
-    nanos_polling_cond_wait( &waitCond );
-
-    return ierror + status.MPI_ERROR;
+    return ierror;
 }
 
 int MPI_Sendrecv( MPI3CONST void *sendbuf, int sendcount, MPI_Datatype sendtype,
@@ -81,20 +65,125 @@ int MPI_Sendrecv( MPI3CONST void *sendbuf, int sendcount, MPI_Datatype sendtype,
                        int source, int recvtag,
                        MPI_Comm comm, MPI_Status *status)
 {
-    int ierror = MPI_SUCCESS;
-    MPI_Request sendreq;
-    MPI_Request recvreq;
-    MPI_Status sendstat;
+    debug0( "Intercepted " << __FUNCTION__ );
+    int isend_error = MPI_SUCCESS;
+    int irecv_error = MPI_SUCCESS;
+    MPI_Request reqlist[2];
 
-    ierror = MPI_Isend( sendbuf, sendcount, sendtype, dest, sendtag, comm, &sendreq );
-    ierror = MPI_Irecv( recvbuf, recvcount, recvtype, source, recvtag, comm, &recvreq );
+    isend_error = MPI_Isend( sendbuf, sendcount, sendtype, dest, sendtag, comm, &reqlist[0] );
+    irecv_error = MPI_Irecv( recvbuf, recvcount, recvtype, source, recvtag, comm, &reqlist[1] );
 
-    nanos::mpi::MultipleTestCond waitCond({sendreq, recvreq}, {sendstat,*status});
-    nanos_polling_cond_wait( &waitCond );
+    if( isend_error == MPI_SUCCESS && irecv_error == MPI_SUCCESS ) {
+        shared_pointer<MPI_MultipleTest> waitCond;
+        if( status == MPI_STATUS_IGNORE ) {
+            waitCond.setPointer( new MPI_MultipleTest(2, reqlist, MPI_STATUSES_IGNORE) );
+            shared_pointer<MPI_MultipleTest> waitCond = shared_pointer<MPI_MultipleTest>(new MPI_MultipleTest(2, reqlist, MPI_STATUSES_IGNORE) );
+            nanos_polling_cond_wait( waitCond.getPointer() );
+        } else {
+            MPI_Status statuslist[2];
+            waitCond.setPointer( new MPI_MultipleTest(2, reqlist, statuslist) );
+            nanos_polling_cond_wait( waitCond.getPointer() );
+            // Copy the value of irecv status back to the status provided by the user
+            *status = statuslist[1];
+        }
+        irecv_error = waitCond->getResult();
+    }
 
-    return ierror + status->MPI_ERROR;
+    return isend_error? isend_error : irecv_error;
 }
 
+int MPI_Sendrecv_replace( void *buf, int count, MPI_Datatype datatype,
+                       int dest, int sendtag,
+                       int source, int recvtag,
+                       MPI_Comm comm, MPI_Status *status)
+{
+    debug0( "Intercepted " << __FUNCTION__ );
+    /*
+     * Note: this implementation tries to mimic its implementation in OpenMPI: 
+     * https://github.com/open-mpi/ompi/blob/master/ompi/mpi/c/sendrecv_replace.c
+     * It creates a temporary buffer where it stores the received message in
+     * raw data. Then, it unpacks it in the original -- user provided -- buffer
+     * and releases the reserved memory.
+     */
+
+    if ( source == MPI_PROC_NULL || dest == MPI_PROC_NULL || count == 0 ) {
+        return MPI_Sendrecv( buf, count, datatype, dest, sendtag,
+                             buf, count, datatype, source, recvtag,
+                             comm, status );
+    } else {
+        int ierror = MPI_SUCCESS;
+        int alloc = MPI_SUCCESS;
+        MPI_Request reqlist[2];
+        int packed_size;
+        void *helperbuf = NULL;
+
+        /* setup a buffer for recv */
+        ierror = MPI_Pack_size( count, datatype, comm, &packed_size );
+        alloc = MPI_Alloc_mem( packed_size, MPI_INFO_NULL, &helperbuf );
+
+        /* recv into temporary buffer */
+        if ( ierror == MPI_SUCCESS  && alloc == MPI_SUCCESS ) {
+            ierror = MPI_Sendrecv( buf, count, datatype, dest, sendtag,
+                           helperbuf, packed_size, MPI_BYTE, source, recvtag,
+                           comm, status);
+        }
+
+        /* copy into users buffer */
+        if ( ierror == MPI_SUCCESS && alloc == MPI_SUCCESS ) {
+            int position = 0;
+            ierror = MPI_Unpack( helperbuf, packed_size, &position,
+                        buf, count, datatype, comm );
+        }
+
+        /* release resources */
+        if ( alloc == MPI_SUCCESS ) {
+            ierror = MPI_Free_mem( helperbuf );
+        }
+        return ierror;
+    }
+}
+
+#if MPI_VERSION >=3
+int MPI_Barrier(MPI_Comm comm)
+{
+    debug0( "Intercepted " << __FUNCTION__ );
+    int ierror = MPI_SUCCESS;
+    MPI_Request request;
+    MPI_Status status;
+
+    ierror = MPI_Ibarrier( comm, &request );
+
+    if( ierror == MPI_SUCCESS ) {
+        shared_pointer<MPI_SingleTest> waitCond = shared_pointer<MPI_SingleTest>(new MPI_SingleTest(&request, &status) );
+        nanos_polling_cond_wait( waitCond.getPointer() );
+
+        // What happens if error is MPI_ERR_IN_STATUS?
+        ierror = waitCond->getResult();
+    }
+
+    return ierror;
+}
+
+int MPI_Bcast(void *buffer, int count, MPI_Datatype datatype,
+    int root, MPI_Comm comm)
+{
+    debug0( "Intercepted " << __FUNCTION__ );
+    int ierror = MPI_SUCCESS;
+    MPI_Request request;
+    MPI_Status status;
+
+    ierror = MPI_Ibcast( buffer, count, datatype, root, comm, &request );
+
+    if( ierror == MPI_SUCCESS ) {
+        shared_pointer<MPI_SingleTest> waitCond = shared_pointer<MPI_SingleTest>(new MPI_SingleTest(&request, &status) );
+        nanos_polling_cond_wait( waitCond.getPointer() );
+
+        // What happens if error is MPI_ERR_IN_STATUS?
+        ierror = waitCond->getResult();
+    }
+
+    return ierror;
+}
 #endif
 
 }// extern C
