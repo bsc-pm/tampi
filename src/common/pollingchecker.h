4da27ac7b513fe584624039ca7aeecf7935fa189
Jorge Bellon <jorge.bellon@bsc.es>
2015-10-06 13:01:07 +0200

    Adding copyright header to all source files.

--- src/common/pollingchecker.h
+++ src/common/pollingchecker.h
@@ -17,6 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #ifndef POLLINGCHECKER_H
 #define POLLINGCHECKER_H
 
