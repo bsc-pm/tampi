6992fda955288a41b89acd86c5bf5157663bdc26
Victor Lopez <victor.lopez@bsc.es>
2016-11-09 10:58:50 +0100

    Implement Alltoall and Alltoallv MPI calls

---
+++ src/c/alltoallv.cc
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2016 Barcelona Supercomputing Center. All rights
+ *                    reserved.
+ *
+ * This file is part of OmpSs-MPI interoperability library.
+ *
+ * OmpSs-MPI interop. library is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <mpi.h> // defines MPI_VERSION
+
+#if MPI_VERSION >=3
+
+#include "mpi/request.h"
+#include "mpi/status.h"
+#include "ticket.h"
+#include "print.h"
+
+using namespace nanos::mpi;
+using ticket_t = Ticket<C::request,C::ignored_status,1>;
+
+extern "C" {
+    int MPI_Alltoallv( const void *sendbuf, const int sendcounts[],
+                        const int sdispls[], MPI_Datatype sendtype,
+                        void *recvbuf, const int recvcounts[],
+                        const int rdispls[], MPI_Datatype recvtype,
+                        MPI_Comm comm )
+    {
+        print::intercepted_call( __func__ );
+
+        C::request req;
+        int err = MPI_Ialltoallv( sendbuf, sendcounts, sdispls, sendtype,
+                                recvbuf, recvcounts, rdispls, recvtype,
+                                comm, &static_cast<MPI_Request&>(req) );
+
+        ticket_t ticket( {req}, err );
+        ticket.wait();
+        return err;
+    }
+}
+
+#endif // MPI_VERSION
---
+++ src/fortran/alltoallv.cc
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2016 Barcelona Supercomputing Center. All rights
+ *                    reserved.
+ *
+ * This file is part of OmpSs-MPI interoperability library.
+ *
+ * OmpSs-MPI interop. library is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <mpi.h> // defines MPI_VERSION
+
+#if MPI_VERSION >=3
+
+#include "mpi/request.h"
+#include "mpi/status.h"
+#include "ticket.h"
+#include "print.h"
+
+using namespace nanos::mpi;
+using ticket_t = Ticket<Fortran::request,Fortran::ignored_status,1>;
+
+extern "C" {
+    void mpi_ialltoallv_( const void *sendbuf, const MPI_Fint sendcounts[],
+                        const MPI_Fint sdispls[], MPI_Fint *sendtype,
+                        void *recvbuf, const MPI_Fint recvcounts[],
+                        const MPI_Fint rdispls[], MPI_Fint *recvtype,
+                        MPI_Fint *comm, MPI_Fint *req, MPI_Fint *ierror );
+
+    void mpi_alltoallv_( const void *sendbuf, const MPI_Fint sendcounts[],
+                        const MPI_Fint sdispls[], MPI_Fint *sendtype,
+                        void *recvbuf, const MPI_Fint recvcounts[],
+                        const MPI_Fint rdispls[], MPI_Fint *recvtype,
+                        MPI_Fint *comm, MPI_Fint *ierror )
+    {
+        print::intercepted_call( __func__ );
+
+        Fortran::request req;
+        mpi_ialltoallv_( sendbuf, sendcounts, sdispls, sendtype,
+                        recvbuf, recvcounts, rdispls, recvtype,
+                        comm, &static_cast<MPI_Fint&>(req), ierror );
+
+        ticket_t ticket( {req}, *ierror );
+        ticket.wait();
+        *ierror = ticket.getReturnError();
+    }
+}
+
+#endif // MPI_VERSION
