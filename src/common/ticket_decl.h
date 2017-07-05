a09377dbcdbab05ed5aee35fad8b1a84af7d8481
Xavier Teruel <xavier.teruel@bsc.es>
2017-07-05 12:51:19 +0200

    Fixing bool comparison

--- src/common/ticket_decl.h
+++ src/common/ticket_decl.h
@@ -60,7 +60,7 @@ struct WaitProperties {
    }
 
    bool spinNotYield() {
-      return _spinForever | (_numSpins--) > 0;
+      return _spinForever || (_numSpins--) > 0;
    }
 
    bool spinForever() const {
@@ -145,14 +145,14 @@ struct Ticket : public detail::TicketBase {
       Status* getStatus() { return _first_status; }
 
       bool ignoreStatus() const {
-         return _first_status == MPI_STATUS_IGNORE | _first_status == MPI_STATUSES_IGNORE;
+         return _first_status == MPI_STATUS_IGNORE || _first_status == MPI_STATUSES_IGNORE;
       }
 
       void notifyCompletion( int status_pos, const Status& status ) {
          if( !ignoreStatus() ) {
             const Status* first = &status;
             const Status* last  = std::next(first);
-            Status* out   = _first_status;
+            Status* out   = std::next(_first_status, status_pos);
             std::copy<const Status*,Status*>( first, last, out );
          }
 	 TicketBase::notifyCompletion();
@@ -179,14 +179,14 @@ struct Ticket : public detail::TicketBase {
 
       bool ignoreStatus() const {
          // MPI fortran API decays status array to a pointer to integer
-         return _first_status == MPI_F_STATUS_IGNORE | _first_status == MPI_F_STATUSES_IGNORE;
+         return _first_status == MPI_F_STATUS_IGNORE || _first_status == MPI_F_STATUSES_IGNORE;
       }
 
       void notifyCompletion( int status_pos, const Status& status ) {
          if( !ignoreStatus() ) {
             const Status* first = &status;
             const Status* last  = std::next(first);
-            Status* out   = reinterpret_cast<Status*>(_first_status);
+            Status* out   = std::next(reinterpret_cast<Status*>(_first_status),status_pos);
             std::copy<const Status*,Status*>( first, last, out );
          }
 	 TicketBase::notifyCompletion();
