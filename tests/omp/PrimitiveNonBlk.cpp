bf75fc4ffb2bf4a5171a10e86eb5a321b0142f6d
Kevin Sala <kevin.sala@bsc.es>
2019-01-25 17:00:52 +0100

    Adding some OpenMP and OmpSs-2 tests.

---
+++ tests/omp/PrimitiveNonBlk.cpp
@@ -0,0 +1,93 @@
+#include <mpi.h>
+#include <TAMPI.h>
+
+#include "Utils.hpp"
+
+#include <vector>
+
+const int TIMESTEPS = 1000;
+const int MSG_NUM = 1000;
+const int MSG_SIZE = 100;
+
+MPI_Status statuses[MSG_NUM];
+
+int main(int argc, char **argv)
+{
+	int provided;
+	const int required = MPI_THREAD_MULTIPLE;
+	CHECK(MPI_Init_thread(&argc, &argv, required, &provided));
+	ASSERT(provided == required);
+	
+	int rank, size;
+	CHECK(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+	CHECK(MPI_Comm_size(MPI_COMM_WORLD, &size));
+	ASSERT(size > 1);
+	
+	int * const buffer = (int *) std::malloc(MSG_NUM * MSG_SIZE * sizeof(int));
+	ASSERT(buffer != nullptr);
+	
+	CHECK(MPI_Barrier(MPI_COMM_WORLD));
+	double startTime = getTime();
+	
+	#pragma omp parallel
+	#pragma omp single
+	{
+		for (int t = 0; t < TIMESTEPS; ++t) {
+			if (rank == 0) {
+				int *message = buffer;
+				
+				for (int m = 0; m < MSG_NUM; ++m) {
+					#pragma omp task depend(out: message[0:MSG_SIZE-1])
+					for (int d = 0; d < MSG_SIZE; ++d) {
+						message[d] = d;
+					}
+					
+					#pragma omp task depend(in: message[0:MSG_SIZE-1])
+					{
+						MPI_Request request;
+						CHECK(MPI_Isend(message, MSG_SIZE, MPI_INT, 1, m, MPI_COMM_WORLD, &request));
+						CHECK(TAMPI_Iwait(&request, MPI_STATUS_IGNORE));
+					}
+					message += MSG_SIZE;
+				}
+			} else if (rank == 1) {
+				int *message = buffer + (MSG_NUM - 1) * MSG_SIZE;
+				
+				for (int m = MSG_NUM - 1; m >= 0; --m) {
+					#pragma omp task depend(out: message[0:MSG_SIZE-1], statuses[m])
+					{
+						MPI_Request request;
+						CHECK(MPI_Irecv(message, MSG_SIZE, MPI_INT, 0, m, MPI_COMM_WORLD, &request));
+						CHECK(TAMPI_Iwait(&request, &statuses[m]));
+					}
+					
+					#pragma omp task depend(in: message[0:MSG_SIZE-1], statuses[m])
+					for (int d = 0; d < MSG_SIZE; ++d) {
+						ASSERT(message[d] == d);
+						ASSERT(statuses[m].MPI_TAG == m);
+						ASSERT(statuses[m].MPI_SOURCE == 0);
+						
+						int count;
+						CHECK(MPI_Get_count(&statuses[m], MPI_INT, &count));
+						ASSERT(count == MSG_SIZE);
+					}
+					message -= MSG_SIZE;
+				}
+			}
+		}
+		#pragma omp taskwait
+	}
+	
+	CHECK(MPI_Barrier(MPI_COMM_WORLD));
+	
+	if (rank == 0) {
+		double endTime = getTime();
+		fprintf(stdout, "Success, time: %f\n", endTime - startTime);
+	}
+	
+	CHECK(MPI_Finalize());
+	
+	std::free(buffer);
+	
+	return 0;
+}
---
+++ tests/ompss2/HugeBlkTasks.cpp
@@ -0,0 +1,88 @@
+#include <mpi.h>
+#include <TAMPI.h>
+
+#include "Utils.hpp"
+
+#include <vector>
+
+const int TIMESTEPS = 1000;
+const int MSG_NUM = 1000;
+const int MSG_SIZE = 100;
+const int TOTAL_SIZE = MSG_NUM * MSG_SIZE;
+
+MPI_Request requests[MSG_NUM];
+MPI_Status statuses[MSG_NUM];
+
+int main(int argc, char **argv)
+{
+	int provided;
+	const int required = MPI_TASK_MULTIPLE;
+	CHECK(MPI_Init_thread(&argc, &argv, required, &provided));
+	ASSERT(provided == required);
+	
+	int rank, size;
+	CHECK(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+	CHECK(MPI_Comm_size(MPI_COMM_WORLD, &size));
+	ASSERT(size > 1);
+	
+	int * const buffer = (int *) std::malloc(MSG_NUM * MSG_SIZE * sizeof(int));
+	ASSERT(buffer != nullptr);
+	
+	CHECK(MPI_Barrier(MPI_COMM_WORLD));
+	double startTime = getTime();
+	
+	for (int t = 0; t < TIMESTEPS; ++t) {
+		if (rank == 0) {
+			#pragma oss task out(buffer[0;TOTAL_SIZE]) label(init)
+			for (int d = 0; d < TOTAL_SIZE; ++d) {
+				buffer[d] = d;
+			}
+			
+			#pragma oss task in(buffer[0;TOTAL_SIZE]) shared(requests) label(send)
+			{
+				for (int m = 0; m < MSG_NUM; ++m) {
+					CHECK(MPI_Isend(&buffer[m*MSG_SIZE], MSG_SIZE, MPI_INT, 1, m, MPI_COMM_WORLD, &requests[m]));
+				}
+				CHECK(MPI_Waitall(MSG_NUM, requests, MPI_STATUSES_IGNORE));
+			}
+		} else if (rank == 1) {
+			int *message = buffer + (MSG_NUM - 1) * MSG_SIZE;
+			
+			#pragma oss task out(buffer[0;TOTAL_SIZE]) shared(requests) shared(statuses) label(recv)
+			{
+				for (int m = MSG_NUM - 1; m >= 0; --m) {
+					CHECK(MPI_Irecv(&buffer[m*MSG_SIZE], MSG_SIZE, MPI_INT, 0, m, MPI_COMM_WORLD, &requests[MSG_NUM-1-m]));
+				}
+				CHECK(MPI_Waitall(MSG_NUM, requests, statuses));
+				
+				for (int m = 0; m < MSG_NUM; ++m) {
+					ASSERT(statuses[m].MPI_TAG == MSG_NUM - 1 - m);
+					ASSERT(statuses[m].MPI_SOURCE == 0);
+					
+					int count;
+					CHECK(MPI_Get_count(&statuses[m], MPI_INT, &count));
+					ASSERT(count == MSG_SIZE);
+				}
+			}
+			
+			#pragma oss task in(buffer[0;TOTAL_SIZE]) label(check)
+			for (int d = 0; d < TOTAL_SIZE; ++d) {
+				ASSERT(buffer[d] == d);
+			}
+		}
+	}
+	#pragma oss taskwait
+	
+	CHECK(MPI_Barrier(MPI_COMM_WORLD));
+	
+	if (rank == 0) {
+		double endTime = getTime();
+		fprintf(stdout, "Success, time: %f\n", endTime - startTime);
+	}
+	
+	CHECK(MPI_Finalize());
+	
+	std::free(buffer);
+	
+	return 0;
+}
