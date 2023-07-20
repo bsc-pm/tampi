e2c16709b0c5f1f6507896607b3e7040c3a06cdd
Kevin Sala <kevin.sala@bsc.es>
2023-07-20 15:35:41 +0200

    Improving TAMPI_Decl.h header

--- src/include/TAMPI_Decl.h
+++ src/include/TAMPI_Decl.h
@@ -1,7 +1,7 @@
 /*
 	This file is part of Task-Aware MPI and is licensed under the terms contained in the COPYING and COPYING.LESSER files.
 
-	Copyright (C) 2015-2019 Barcelona Supercomputing Center (BSC)
+	Copyright (C) 2015-2023 Barcelona Supercomputing Center (BSC)
 */
 
 #ifndef TAMPI_DECL_H
@@ -11,22 +11,26 @@
 
 #pragma GCC visibility push(default)
 
-//! MPI Threading level to enable the blocking mode of TAMPI
-#define MPI_TASK_MULTIPLE (MPI_THREAD_MULTIPLE + 1)
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+//! The MPI thread level that enables the task-aware blocking mode
+#define MPI_TASK_MULTIPLE (MPI_THREAD_MULTIPLE + 1)
+
+//! Functions to know which TAMPI modes are enabled
 int TAMPI_Blocking_enabled(int *flag);
 int TAMPI_Nonblocking_enabled(int *flag);
-int TAMPI_Iwait(MPI_Request *request, MPI_Status *status);
-int TAMPI_Iwaitall(int count, MPI_Request array_of_requests[], MPI_Status array_of_statuses[]);
 
+//! Functions to asycnhronously wait communications from tasks
+int TAMPI_Iwait(MPI_Request *request, MPI_Status *status);
+int TAMPI_Iwaitall(int count, MPI_Request requests[], MPI_Status statuses[]);
+
+//! Fortran prototypes of previous C/C++ functions
 void tampi_blocking_enabled_(MPI_Fint *flag, MPI_Fint *err);
 void tampi_nonblocking_enabled_(MPI_Fint *flag, MPI_Fint *err);
 void tampi_iwait_(MPI_Fint *request, MPI_Fint *status, MPI_Fint *err);
-void tampi_iwaitall_(MPI_Fint *count, MPI_Fint array_of_requests[], MPI_Fint *array_of_statuses, MPI_Fint *err);
+void tampi_iwaitall_(MPI_Fint *count, MPI_Fint requests[], MPI_Fint *statuses, MPI_Fint *err);
 
 #ifdef __cplusplus
 }
